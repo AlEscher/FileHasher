b955feb74acec176b22a418b747916ccd4401776
AlEscher <pinco-pallino@gmx.net>
2021-03-13 18:44:54 +0100

    Added error checking to #7 - Now parsing and verifying JSON before saving it to a file - Added error messages to MessageBoxes

--- GUI/FileHasherUI/exportdialog.cpp
+++ GUI/FileHasherUI/exportdialog.cpp
@@ -5,6 +5,8 @@
 #include <QTimer>
 #include <QFileDialog>
 #include <QMessageBox>
+#include <QJsonDocument>
+#include <QJsonParseError>
 
 ExportDialog::ExportDialog(QWidget *parent, FileHasherDelegate* delegate) : QDialog(parent), ui(new Ui::exportDialog)
 {
@@ -39,16 +41,26 @@ void ExportDialog::on_saveAsBtn_clicked()
     const QString filter = m_bIsJson ? "JSON (*.json)" : "Text files (*.txt)";
     // Get the desired path from the user
     const QString filePath = QFileDialog::getSaveFileName(this, "Save as", "", filter);
-    const QString content = ui->exportedText->toPlainText();
+    const QByteArray content = ui->exportedText->toPlainText().toUtf8();
+
+    // Check that the JSON is correctly formatted in case the user made any changes manually
+    QJsonParseError jsonError;
+    QJsonDocument::fromJson(content, &jsonError);
+    if (jsonError.error != QJsonParseError::NoError)
+    {
+        QMessageBox::warning(this, "Invalid JSON", QString("The exported JSON string is invalid\nUndo any manual changes or export it again to fix this warning:\n\"%1\"")
+                             .arg(jsonError.errorString()));
+        return;
+    }
 
     QFile newFile = QFile(filePath);
     if (!newFile.open(QIODevice::WriteOnly))
     {
-        QMessageBox::warning(this, "Error", "Couldn't create the requested file");
+        QMessageBox::critical(this, "File Error", QString("Couldn't create the requested file:\n\"%1\"").arg(newFile.errorString()));
         return;
     }
 
-    newFile.write(content.toUtf8());
+    newFile.write(content);
     newFile.close();
 }
 
