16ff6f5b7050d5419c98b6dfbb245cc798657b58
AlEscher <alessandro.escher@tum.de>
2021-05-23 12:00:27 +0200

    Using Macro to create Tests

--- Testing/FileHasherCI_Test/test.cpp
+++ Testing/FileHasherCI_Test/test.cpp
@@ -15,52 +15,28 @@
 #define RANDOMBYTES_TEST_FILE TEST_FILES_DIR L"random_bytes"
 #define NONEXISTANT_TEST_FILE TEST_FILES_DIR L"IDONTEXIST.ZZZZZ"
 
+#define CREATE_HASH_TEST(TestSuite, TestName, Hasher, ExpectedHash, File)\
+	TEST(TestSuite, TestName)\
+	{\
+	HashingAlgorithm* hasher = new Hasher;\
+	const std::string expectedHash = ExpectedHash;\
+	const std::string actualHash = hasher->CalculateFileHash(File);\
+	EXPECT_EQ(actualHash, expectedHash);\
+	delete hasher;\
+	}
+
 /**
 * ===================
 * |	  MD5HashTest	|
 * ===================
 */
 
-TEST(MD5HashTest, HandleEmptyFile) 
-{
-	HashingAlgorithm* md5Hasher = new MD5Hasher;
-	const std::string expectedHash = "d41d8cd98f00b204e9800998ecf8427e";
-	const std::string actualHash = md5Hasher->CalculateFileHash(EMPTY_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete md5Hasher;
-}
-TEST(MD5HashTest, HandleBrownFox)
-{
-	HashingAlgorithm* md5Hasher = new MD5Hasher;
-	const std::string expectedHash = "9e107d9d372bb6826bd81d3542a419d6";
-	const std::string actualHash = md5Hasher->CalculateFileHash(QUICKFOX_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete md5Hasher;
-}
-TEST(MD5HashTest, HandleRandomText)
-{
-	HashingAlgorithm* md5Hasher = new MD5Hasher;
-	const std::string expectedHash = "ef4b6ce35d28359b801810d7981a1bed";
-	const std::string actualHash = md5Hasher->CalculateFileHash(RANDOMTEXT_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete md5Hasher;
-}
-TEST(MD5HashTest, HandleRandomBytes)
-{
-	HashingAlgorithm* md5Hasher = new MD5Hasher;
-	const std::string expectedHash = "a8c9ab390419f00a90e45184a07d55c8";
-	const std::string actualHash = md5Hasher->CalculateFileHash(RANDOMBYTES_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete md5Hasher;
-}
-TEST(MD5HashTest, HandleNonExistant)
-{
-	HashingAlgorithm* md5Hasher = new MD5Hasher;
-	const std::string expectedHash = "";
-	const std::string actualHash = md5Hasher->CalculateFileHash(NONEXISTANT_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete md5Hasher;
-}
+
+CREATE_HASH_TEST(MD5HashTest, HandleEmptyFile, MD5Hasher, "d41d8cd98f00b204e9800998ecf8427e", EMPTY_TEST_FILE)
+CREATE_HASH_TEST(MD5HashTest, HandleBrownFox, MD5Hasher, "9e107d9d372bb6826bd81d3542a419d6", QUICKFOX_TEST_FILE)
+CREATE_HASH_TEST(MD5HashTest, HandleRandomText, MD5Hasher, "ef4b6ce35d28359b801810d7981a1bed", RANDOMTEXT_TEST_FILE)
+CREATE_HASH_TEST(MD5HashTest, HandleRandomBytes, MD5Hasher, "a8c9ab390419f00a90e45184a07d55c8", RANDOMBYTES_TEST_FILE)
+CREATE_HASH_TEST(MD5HashTest, HandleNonExistant, MD5Hasher, "", NONEXISTANT_TEST_FILE)
 
 /**
 * ===================
@@ -68,46 +44,11 @@ TEST(MD5HashTest, HandleNonExistant)
 * ===================
 */
 
-TEST(SHA1HashTest, HandleEmptyFile)
-{
-	HashingAlgorithm* sha1Hasher = new SHA1Hasher;
-	const std::string expectedHash = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
-	const std::string actualHash = sha1Hasher->CalculateFileHash(EMPTY_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha1Hasher;
-}
-TEST(SHA1HashTest, HandleBrownFox)
-{
-	HashingAlgorithm* sha1Hasher = new SHA1Hasher;
-	const std::string expectedHash = "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12";
-	const std::string actualHash = sha1Hasher->CalculateFileHash(QUICKFOX_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha1Hasher;
-}
-TEST(SHA1HashTest, HandleRandomText)
-{
-	HashingAlgorithm* sha1Hasher = new SHA1Hasher;
-	const std::string expectedHash = "e6f65bf47f6e6295d96f6819d7b03cf1c0c99020";
-	const std::string actualHash = sha1Hasher->CalculateFileHash(RANDOMTEXT_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha1Hasher;
-}
-TEST(SHA1HashTest, HandleRandomBytes)
-{
-	HashingAlgorithm* sha1Hasher = new SHA1Hasher;
-	const std::string expectedHash = "978a9a9d686100c11b9198605594ce17434dcbb8";
-	const std::string actualHash = sha1Hasher->CalculateFileHash(RANDOMBYTES_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha1Hasher;
-}
-TEST(SHA1HashTest, HandleNonExistant)
-{
-	HashingAlgorithm* sha1Hasher = new SHA1Hasher;
-	const std::string expectedHash = "";
-	const std::string actualHash = sha1Hasher->CalculateFileHash(NONEXISTANT_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha1Hasher;
-}
+CREATE_HASH_TEST(SHA1HashTest, HandleEmptyFile, SHA1Hasher, "da39a3ee5e6b4b0d3255bfef95601890afd80709", EMPTY_TEST_FILE)
+CREATE_HASH_TEST(SHA1HashTest, HandleBrownFox, SHA1Hasher, "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12", QUICKFOX_TEST_FILE)
+CREATE_HASH_TEST(SHA1HashTest, HandleRandomText, SHA1Hasher, "e6f65bf47f6e6295d96f6819d7b03cf1c0c99020", RANDOMTEXT_TEST_FILE)
+CREATE_HASH_TEST(SHA1HashTest, HandleRandomBytes, SHA1Hasher, "978a9a9d686100c11b9198605594ce17434dcbb8", RANDOMBYTES_TEST_FILE)
+CREATE_HASH_TEST(SHA1HashTest, HandleNonExistant, SHA1Hasher, "", NONEXISTANT_TEST_FILE)
 
 /**
 * ===================
@@ -115,46 +56,11 @@ TEST(SHA1HashTest, HandleNonExistant)
 * ===================
 */
 
-TEST(SHA256HashTest, HandleEmptyFile)
-{
-	HashingAlgorithm* sha256Hasher = new SHA256Hasher;
-	const std::string expectedHash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
-	const std::string actualHash = sha256Hasher->CalculateFileHash(EMPTY_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha256Hasher;
-}
-TEST(SHA256HashTest, HandleBrownFox)
-{
-	HashingAlgorithm* sha256Hasher = new SHA256Hasher;
-	const std::string expectedHash = "d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592";
-	const std::string actualHash = sha256Hasher->CalculateFileHash(QUICKFOX_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha256Hasher;
-}
-TEST(SHA256HashTest, HandleRandomText)
-{
-	HashingAlgorithm* sha256Hasher = new SHA256Hasher;
-	const std::string expectedHash = "a9ad52174f5da3281a58ff7805ed73622512dbe8b4e31e8f6920992ef583919c";
-	const std::string actualHash = sha256Hasher->CalculateFileHash(RANDOMTEXT_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha256Hasher;
-}
-TEST(SHA256HashTest, HandleRandomBytes)
-{
-	HashingAlgorithm* sha256Hasher = new SHA256Hasher;
-	const std::string expectedHash = "c089c3f4fafda89a17edbb9d87cde4a5ff3ac0c3749d88441dc09e04f427410f";
-	const std::string actualHash = sha256Hasher->CalculateFileHash(RANDOMBYTES_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha256Hasher;
-}
-TEST(SHA256HashTest, HandleNonExistant)
-{
-	HashingAlgorithm* sha256Hasher = new SHA256Hasher;
-	const std::string expectedHash = "";
-	const std::string actualHash = sha256Hasher->CalculateFileHash(NONEXISTANT_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha256Hasher;
-}
+CREATE_HASH_TEST(SHA256HashTest, HandleEmptyFile, SHA256Hasher, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", EMPTY_TEST_FILE)
+CREATE_HASH_TEST(SHA256HashTest, HandleBrownFox, SHA256Hasher, "d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592", QUICKFOX_TEST_FILE)
+CREATE_HASH_TEST(SHA256HashTest, HandleRandomText, SHA256Hasher, "a9ad52174f5da3281a58ff7805ed73622512dbe8b4e31e8f6920992ef583919c", RANDOMTEXT_TEST_FILE)
+CREATE_HASH_TEST(SHA256HashTest, HandleRandomBytes, SHA256Hasher, "c089c3f4fafda89a17edbb9d87cde4a5ff3ac0c3749d88441dc09e04f427410f", RANDOMBYTES_TEST_FILE)
+CREATE_HASH_TEST(SHA256HashTest, HandleNonExistant, SHA256Hasher, "", NONEXISTANT_TEST_FILE)
 
 /**
 * ===================
@@ -162,43 +68,8 @@ TEST(SHA256HashTest, HandleNonExistant)
 * ===================
 */
 
-TEST(SHA512HashTest, HandleEmptyFile)
-{
-	HashingAlgorithm* sha512Hasher = new SHA512Hasher;
-	const std::string expectedHash = "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e";
-	const std::string actualHash = sha512Hasher->CalculateFileHash(EMPTY_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha512Hasher;
-}
-TEST(SHA512HashTest, HandleBrownFox)
-{
-	HashingAlgorithm* sha512Hasher = new SHA512Hasher;
-	const std::string expectedHash = "07e547d9586f6a73f73fbac0435ed76951218fb7d0c8d788a309d785436bbb642e93a252a954f23912547d1e8a3b5ed6e1bfd7097821233fa0538f3db854fee6";
-	const std::string actualHash = sha512Hasher->CalculateFileHash(QUICKFOX_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha512Hasher;
-}
-TEST(SHA512HashTest, HandleRandomText)
-{
-	HashingAlgorithm* sha512Hasher = new SHA512Hasher;
-	const std::string expectedHash = "64fe2a7304ed70fd6fddebb2dd391bcea3a6bffa5b885484844db1bfcbad1bfcd71e7558ada0d851f302eae6a3f0b48bdb8a34e4154278242ea1c48e617552d8";
-	const std::string actualHash = sha512Hasher->CalculateFileHash(RANDOMTEXT_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha512Hasher;
-}
-TEST(SHA512HashTest, HandleRandomBytes)
-{
-	HashingAlgorithm* sha512Hasher = new SHA512Hasher;
-	const std::string expectedHash = "78e0c313f2ea2af003df177872d8a94553ceb1da000764bd4232294915df62da671fdd292c28b71e0287a1fda09d5e748704037ec6e9c2d6099a0ed06dc12b9e";
-	const std::string actualHash = sha512Hasher->CalculateFileHash(RANDOMBYTES_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha512Hasher;
-}
-TEST(SHA512HashTest, HandleNonExistant)
-{
-	HashingAlgorithm* sha512Hasher = new SHA512Hasher;
-	const std::string expectedHash = "";
-	const std::string actualHash = sha512Hasher->CalculateFileHash(NONEXISTANT_TEST_FILE);
-	EXPECT_EQ(actualHash, expectedHash);
-	delete sha512Hasher;
-}
+CREATE_HASH_TEST(SHA512HashTest, HandleEmptyFile, SHA512Hasher, "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e", EMPTY_TEST_FILE)
+CREATE_HASH_TEST(SHA512HashTest, HandleBrownFox, SHA512Hasher, "07e547d9586f6a73f73fbac0435ed76951218fb7d0c8d788a309d785436bbb642e93a252a954f23912547d1e8a3b5ed6e1bfd7097821233fa0538f3db854fee6", QUICKFOX_TEST_FILE)
+CREATE_HASH_TEST(SHA512HashTest, HandleRandomText, SHA512Hasher, "64fe2a7304ed70fd6fddebb2dd391bcea3a6bffa5b885484844db1bfcbad1bfcd71e7558ada0d851f302eae6a3f0b48bdb8a34e4154278242ea1c48e617552d8", RANDOMTEXT_TEST_FILE)
+CREATE_HASH_TEST(SHA512HashTest, HandleRandomBytes, SHA512Hasher, "78e0c313f2ea2af003df177872d8a94553ceb1da000764bd4232294915df62da671fdd292c28b71e0287a1fda09d5e748704037ec6e9c2d6099a0ed06dc12b9e", RANDOMBYTES_TEST_FILE)
+CREATE_HASH_TEST(SHA512HashTest, HandleNonExistant, SHA512Hasher, "", NONEXISTANT_TEST_FILE)
